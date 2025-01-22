7477bce199ba9d1b125856c496da5be93c9a1b3d
rhsnake <rhsnake@gmail.com>
2025-01-23 01:16:58 +0800

    Added FPS limit

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ pygame.init()
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     game_status = True
+    
     
     while game_status:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_status = False
-            screen.fill(BACKGROUND_COLOR)
-            pygame.display.update()
-            
-            
-    pygame.quit()
-    
+        
+        #Refeshes Background color       
+        screen.fill(BACKGROUND_COLOR)
+        pygame.display.update()
+        #sets fps to 60
+        dt = clock.tick(60) / 1000    
+
     
     
     
