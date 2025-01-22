3884a3f109168a29a43c5d010d98521199e4e604
rhsnake <rhsnake@gmail.com>
2025-01-23 00:58:11 +0800

    Added pygame screen to main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pygame
+from constants import *
+pygame.init()
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_status = True
+    
+    while game_status:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_status = False
+            screen.fill(BACKGROUND_COLOR)
+            pygame.display.update()
+            
+            
+    pygame.quit()
+    
+    
+    
     
     
 if __name__ == "__main__":
